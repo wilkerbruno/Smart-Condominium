72c65aca2a883c1a7084b3e5a18d6471fe0828c4
Wilker Gandra <wilkerbruno@users.noreply.github.com>
2024-06-28 16:51:12 -0300

    alteração do Smart_condominium.py
    Atualizando as rotas das devidas paginas

--- condominio/smart_condominium.py
+++ condominio/smart_condominium.py
@@ -14,14 +14,14 @@ def index():
     return render_template("index.html", titulo="Smart condominium | Home")
 
 
-@app.route("/contato")
-def contato():
-    return render_template("contato.html", titulo="Ajuda")
+@app.route("/cadastro_condominio")
+def cadastro_condominio():
+    return render_template("cadastro_condominio.html", titulo="Cadastro de Condominios")
 
 
-@app.route("/cuidados")
-def cuidados():
-    return render_template("cuidados.html", titulo="Cuidados")
+@app.route("/cadastro_funcionario")
+def cadastro_funcionario():
+    return render_template("cadastro_funcionario.html", titulo="Cadastro_ de Funcionario")
 
 
 @app.route("/sobre")
@@ -29,9 +29,46 @@ def sobre():
     return render_template("sobre.html", titulo="Sobre")
 
 
-@app.route("/inscrever")
-def inscrever():
-    return render_template("inscrever.html", titulo="inscrever")
+@app.route("/cadastro_morador")
+def cadastro_morador():
+    return render_template("cadastro_morador.html", titulo="Cadastro de Morador")
+
+@app.route("/cadastro_unit")
+def cadastro_unit():
+    return render_template("cadastro_unit.html", titulo="cadastro de Unidades")
+
+@app.route("/cadastro_veiculo")
+def cadastro_veiculo():
+    return render_template("cadastro_veiculo.html", titulo="Cadastro de Veiculos")
+
+@app.route("/cobranca")
+def cobranca():
+    return render_template("cobranca.html", titulo="Cobranças")
+
+@app.route("/condominio")
+def condominio():
+    return render_template("condominio.html", titulo="Condominio")
+
+@app.route("/fazer_reservas")
+def fazer_reservas():
+    return render_template("fazer_reservas.html", titulo="Fazer Reservas")
+
+@app.route("/funcionarios")
+def funcionarios():
+    return render_template("funcionarios.html", titulo="Funcionarios")
+
+@app.route("/reservas")
+def reservas():
+    return render_template("reservas.html", titulo="Reservas")
+
+@app.route("/residents")
+def residents():
+    return render_template("residents.html", titulo="Residents")
+
+@app.route("/resultados")
+def resultados():
+    return render_template("resultados.html", titulo="Resultados")
+
 
 
 @app.route("/login")
