6141df85a1dc2aacf76ffc18e5a6146c3ea78d46
douglas0098 <douglas0098henrique2@gmail.com>
2024-07-10 16:24:25 -0300

    Atualizados valores na tabela condominio

--- condominio/models/condominio_model.py
+++ condominio/models/condominio_model.py
@@ -4,18 +4,18 @@ from condominio import db
 class Condominio(db.Model):
     __tablename__ = 'condominio'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(20), nullable=False)
+    nome = db.Column(db.String(50), nullable=False)
     cep = db.Column(db.Integer, nullable=False)
     estado = db.Column(db.String(20), nullable=False)
-    cidade = db.Column(db.String(20), nullable=False)
-    endereco = db.Column(db.String(20), nullable=False)
+    cidade = db.Column(db.String(35), nullable=False)
+    endereco = db.Column(db.String(50), nullable=False)
     numero = db.Column(db.Integer, nullable=False)
     celular = db.Column(db.Integer, nullable=False)
     telefone = db.Column(db.Integer)
-    email = db.Column(db.String(20), unique=True, nullable=False)
-    nome_sindico = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    nome_sindico = db.Column(db.String(50), nullable=False)
     telefone_sindico = db.Column(db.Integer, nullable=False)
-    email_sindico = db.Column(db.String(20), unique=True, nullable=False)
+    email_sindico = db.Column(db.String(50), unique=True, nullable=False)
 
     def __init__(self, nome, cep, estado, cidade, endereco, numero, celular, telefone, email, nome_sindico,
                  telefone_sindico, email_sindico):
